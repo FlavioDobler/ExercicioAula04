d8802c8b2818c5968256443530832d63ee5a566b
Flavio Dobler <flaviodobler2@gmail.com>
2018-11-29 11:51:14 -0200

    {Exercicio1}Aula04

---
+++ Exercicio1.sql
@@ -0,0 +1,5 @@
+/*Exercicio 1*/ 
+use mydb
+select * from uf
+
+insert into uf (nome) values ('SP'),('PR'),('SC'),('MS'),('MT'),('PA'),('AC'),('AL'),('AP'),('AM'),('MG'),('DF'),('CE'),('BA'),('PI'),('RJ'),('RN'),('RS'),('RO'),('RR'),('SE'),('TO'),('GO'),('PB');
